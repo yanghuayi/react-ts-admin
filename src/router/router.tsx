4adde0110642769210b0d5a7f7d424d8ef13a815
cqmimi_xiaoyang <cqmimi_xiaoyang@git.cloud.tencent.com>
2018-11-20 23:49:43 +0800

    lazy router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,14 +1,16 @@
 import { LocaleProvider } from 'antd';
 import zhCN from 'antd/lib/locale-provider/zh_CN'
-import * as React from 'react';
+import React, { lazy, Suspense  } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import App from "../App";
 
-import Dashboard from '../pages/dashboard/index';
+const asyncComponent = (name: string) => {
+  return lazy(() => import(`../pages/${name}`));
+}
 
 const routes: Routers[] = [
   {
-    component: Dashboard,
+    component: asyncComponent('dashboard/index'),
     path: '/dashboard',
   }
 ];
@@ -17,14 +19,16 @@ const AppRouter = () => (
   <LocaleProvider locale={zhCN}>
     <BrowserRouter>
       <App>
-        <Switch>
-          <Route exact={true} path="/" render={() => <Redirect to={{ pathname: '/dashboard' }} />} />
-          {
-            routes.map((item) => (
-              <Route exact={true} path={item.path} component={item.component} />
-            ))
-          }
-        </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact={true} path="/" render={() => <Redirect to={{ pathname: '/dashboard' }} />} />
+            {
+              routes.map((item) => (
+                <Route exact={true} path={item.path} component={item.component} />
+              ))
+            }
+          </Switch>
+        </Suspense>
       </App>
     </BrowserRouter>
   </LocaleProvider>
